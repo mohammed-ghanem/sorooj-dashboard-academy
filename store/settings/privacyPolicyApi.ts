99002e3b099ea24b691f71872a3451c7ec656b66
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2026-02-22 05:13:48 +0200

    setting-privacy

--- store/settings/privacyPolicyApi.ts
+++ store/settings/privacyPolicyApi.ts
@@ -16,12 +16,12 @@ export const privacyPolicyApi = createApi({
     getPrivacyPolicy: builder.query<SettingValue, void>({
       query: () => ({
         url: "/settings",
-        method: "post",
+        method: "get",
         params: { key: "privacy-policy" },
       }),
-      transformResponse: (response: any) =>
-        response.data.setting.value,
-      providesTags: ["PrivacyPolicy"],
+      transformResponse: (response: any) => {
+        return response?.data?.data?.[0]?.value ?? { ar: "", en: "" };
+      },
       keepUnusedDataFor: 300, // 5 دقائق
     }),
 
@@ -34,9 +34,11 @@ export const privacyPolicyApi = createApi({
         method: "post",
         params: { key: "privacy-policy" },
         data: { value },
+        auth: true,
+        withCsrf: true,
       }),
       transformResponse: (response: any) =>
-        response.data.setting.value,
+        response.data.data[0]?.value,
       invalidatesTags: ["PrivacyPolicy"],
     }),
   }),
@@ -46,3 +48,62 @@ export const {
   useGetPrivacyPolicyQuery,
   useUpdatePrivacyPolicyMutation,
 } = privacyPolicyApi;
+
+
+
+
+
+
+
+
+
+
+
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// import { createApi } from "@reduxjs/toolkit/query/react";
+// import { axiosBaseQuery } from "../base/axiosBaseQuery";
+
+// interface SettingValue {
+//   ar: string;
+//   en?: string;
+//   message?: string;
+// }
+
+// export const privacyPolicyApi = createApi({
+//   reducerPath: "privacyPolicyApi",
+//   baseQuery: axiosBaseQuery(),
+//   tagTypes: ["PrivacyPolicy"],
+//   endpoints: (builder) => ({
+//     getPrivacyPolicy: builder.query<SettingValue, void>({
+//       query: () => ({
+//         url: "/settings",
+//         method: "post",
+//         params: { key: "privacy-policy" },
+//       }),
+//       transformResponse: (response: any) =>
+//         response.data.setting.value,
+//       providesTags: ["PrivacyPolicy"],
+//       keepUnusedDataFor: 300, // 5 دقائق
+//     }),
+
+//     updatePrivacyPolicy: builder.mutation<
+//       SettingValue,
+//       SettingValue
+//     >({
+//       query: (value) => ({
+//         url: "/settings",
+//         method: "post",
+//         params: { key: "privacy-policy" },
+//         data: { value },
+//       }),
+//       transformResponse: (response: any) =>
+//         response.data.setting.value,
+//       invalidatesTags: ["PrivacyPolicy"],
+//     }),
+//   }),
+// });
+
+// export const {
+//   useGetPrivacyPolicyQuery,
+//   useUpdatePrivacyPolicyMutation,
+// } = privacyPolicyApi;
