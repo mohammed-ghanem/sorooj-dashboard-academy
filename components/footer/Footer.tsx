462aa2ce2471731d8e1e2e273dc68a0cb0e89791
Heba-Hassan-Mostafa <hebahassan3093@gmail.com>
2026-03-09 00:32:33 +0200

    test-push-heba

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <footer className="w-full border-t bg-white py-3 px-4 text-sm text-gray-500">
             <div className="flex flex-col md:flex-row items-center justify-center gap-1 md:gap-2 text-center">
                 <span>
-                    {translate?.footer?.copyright} © 2026 - {currentYear}
+                    {translate?.footer?.copyright} © 2026 -- {currentYear}
                 </span>
 
             </div>
